aff9f16e37b0311772928bd8dc76971d50e6a7d1
Amedomaro <30287338+amedomaro@users.noreply.github.com>
2020-04-30 20:09:51 +0300

    Add 6 kyu Sum of Digits / Digital Root

---
+++ src/kata/kyu6/SumOfDigits/DRoot.java
@@ -0,0 +1,18 @@
+package kata.kyu6.SumOfDigits;
+
+public class DRoot {
+
+    public static int digital_root(int n) {
+        int sum = 0;
+
+        while (n * 0.1 != 0) {
+            sum += (n % 10);
+            n = (int) (n * 0.1);
+        }
+
+        if ((int)(sum * 0.1) != 0) {
+            return digital_root(sum);
+        }
+        return sum;
+    }
+}
---
+++ src/kata/kyu6/SumOfDigits/Main.java
@@ -0,0 +1,7 @@
+package kata.kyu6.SumOfDigits;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(DRoot.digital_root(132189));
+    }
+}
