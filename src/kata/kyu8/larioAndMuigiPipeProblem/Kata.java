ce1f5fed1e37bde26dc6859016ee93724a7a036e
Amedomaro <amedomaro13@gmail.com>
2021-01-31 13:03:29 +0300

    Solution

--- src/kata/kyu8/larioAndMuigiPipeProblem/Kata.java
+++ src/kata/kyu8/larioAndMuigiPipeProblem/Kata.java
@@ -1,4 +1,11 @@
 package kata.kyu8.larioAndMuigiPipeProblem;
 
 public class Kata {
+    public static int[] pipeFix(int[] numbers) {
+        int [] result = new int[(numbers[numbers.length-1] - numbers[0]) + 1];
+        for (int i = 0, j = numbers[0]; i < result.length; i++, j++){
+            result[i] = j;
+        }
+        return result;
+    }
 }
---
+++ src/kata/kyu8/larioAndMuigiPipeProblem/Test.java
@@ -0,0 +1,9 @@
+package kata.kyu8.larioAndMuigiPipeProblem;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(Kata.pipeFix(new int[] {1,2,3,5,6,8,9})));
+    }
+}
