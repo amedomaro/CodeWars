d5259aee5f1fb94f33a63e971ad1e6858740f70a
Amedomaro <amedomaro13@gmail.com>
2021-02-01 12:07:47 +0300

    Solution

--- src/kata/kyu7/alphabetWar/Kata.java
+++ src/kata/kyu7/alphabetWar/Kata.java
@@ -1,7 +1,36 @@
 package kata.kyu7.alphabetWar;
 
-public class Kata{
-    public static String alphabetWar(String fight){
-        return "Let's fight again!";
+import java.util.HashMap;
+import java.util.Map;
+
+public class Kata {
+
+    private static Map<Character, Integer> leftMap = new HashMap();
+    private static Map<Character, Integer> rightMap = new HashMap();
+
+    static {
+        leftMap.put('w', 4);
+        leftMap.put('p', 3);
+        leftMap.put('b', 2);
+        leftMap.put('s', 1);
+        rightMap.put('m', 4);
+        rightMap.put('q', 3);
+        rightMap.put('d', 2);
+        rightMap.put('z', 1);
+    }
+
+    public static String alphabetWar(String fight) {
+        int left = 0, right = 0;
+
+        for (char ch : fight.toCharArray()) {
+            if (leftMap.containsKey(ch)) {
+                left += leftMap.get(ch);
+            }
+            if (rightMap.containsKey(ch)) {
+                right += rightMap.get(ch);
+            }
+        }
+
+        return (left == right) ? "Let's fight again!" : (left < right) ? "Right side wins!" : "Left side wins!";
     }
 }
