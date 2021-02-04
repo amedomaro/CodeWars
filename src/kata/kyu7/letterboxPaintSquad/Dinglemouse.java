f2ad5aa0925254d997b74deeea04861f0fa777dc
Amedomaro <amedomaro13@gmail.com>
2021-02-04 09:21:00 +0300

    Solution

--- src/kata/kyu7/letterboxPaintSquad/Dinglemouse.java
+++ src/kata/kyu7/letterboxPaintSquad/Dinglemouse.java
@@ -3,8 +3,16 @@ package kata.kyu7.letterboxPaintSquad;
 public class Dinglemouse {
 
     public static int[] paintLetterboxes(final int start, final int end) {
-        // Your code here
-        return new int[10];
-    }
+        int[] result = new int[10];
 
+        for(int i = start; i <= end; i++){
+            int num = i;
+
+            while (num > 0){
+                result[num % 10] += 1;
+                num /= 10;
+            }
+        }
+        return result;
+    }
 }
---
+++ src/kata/kyu7/letterboxPaintSquad/Test.java
@@ -0,0 +1,10 @@
+package kata.kyu7.letterboxPaintSquad;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(Dinglemouse.paintLetterboxes(125, 132)));
+        //System.out.println(10%3);
+    }
+}
