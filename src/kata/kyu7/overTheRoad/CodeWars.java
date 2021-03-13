d245d1df5c6427fdeaebb0d747b32f3a6a0025bc
Amedomaro <amedomaro13@gmail.com>
2021-03-13 21:29:24 +0300

    Solution

--- src/kata/kyu7/overTheRoad/CodeWars.java
+++ src/kata/kyu7/overTheRoad/CodeWars.java
@@ -2,7 +2,11 @@ package kata.kyu7.overTheRoad;
 
 class CodeWars {
     public static long overTheRoad(long address, long n) {
-        // Good luck
-        return 0;
+        long home = (n * 2) - ((address / 2) + (address / 2));
+
+        if (address % 2 != 0){
+            return home;
+        }
+        return home + 1;
     }
 }
